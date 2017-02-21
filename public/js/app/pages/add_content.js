2e3be72dfb5673dd89b1a12c7c6cb5448a993906
Binod Chaudhary <thebinod7@gmail.com>
2017-02-21 10:33:19 +0545

    Add content validation, ckeditor congig

--- public/js/app/pages/add_content.js
+++ public/js/app/pages/add_content.js
@@ -1,29 +1,36 @@
 $(document).ready(function() {
     $('#btnAddConent').on("click", function () {
         var data = CKEDITOR.instances.blog_content.getData();
-        var model = {
-            title: $('#title').val(),
-            blogContent: data
-        };
-        $.ajax({
-            method: 'POST',
-            data: model,
-            url: 'cms/addContent',
-            success: function (data) {
-                console.log(data);
-                if(data){
-                    $( "#msg" ).html( '<p class="edu-green"><strong>Content Added Successfully.</strong></p>' );
-                    clearForm();
+        var title = $('#title').val();
+        if(title == ''){
+            $( "#msg" ).html( '<p class="text-danger"><strong>Please enter blog title and content.</strong></p>' );
+            return;
+        }
+        else {
+            var model = {
+                title: title,
+                blogContent: data
+            };
+            $.ajax({
+                method: 'POST',
+                data: model,
+                url: 'cms/addContent',
+                success: function (data) {
+                    console.log(data);
+                    if(data){
+                        $( "#msg" ).html( '<p class="edu-green"><strong>Content Added Successfully.</strong></p>' );
+                        clearForm();
+                    }
+                    else {
+                        $( "#msg" ).html( '<p class="edu-green"><strong>Something went wrogn, plese try again.</strong></p>' );
+                    }
+                },
+                error: function(err) {
+                    console.log(err);
+                    $( "#msg" ).html( '<p class="text-danger"><strong>Incorrect login details. Forgot Password? or Sign up for an account?</strong></p>' );
                 }
-                else {
-                    $( "#msg" ).html( '<p class="edu-green"><strong>Something went wrogn, plese try again.</strong></p>' );
-                }
-            },
-            error: function(err) {
-                console.log(err);
-                $( "#msg" ).html( '<p class="text-danger"><strong>Incorrect login details. Forgot Password? or Sign up for an account?</strong></p>' );
-            }
-        });
+            });
+        }
     });
 });
 
--- public/js/vendors/ckeditor/config.js
+++ public/js/vendors/ckeditor/config.js
@@ -26,6 +26,9 @@ CKEDITOR.editorConfig = function( config ) {
 		{ name: 'about' }
 	];
 
+    config.extraPlugins = 'imageuploader';
+    config.imageUploadUrl = '/imguploader/imgupload.php';
+
 	// Remove some buttons provided by the standard plugins, which are
 	// not needed in the Standard(s) toolbar.
 	config.removeButtons = 'Underline,Subscript,Superscript';
