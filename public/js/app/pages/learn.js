e43b2b499d5baef772f7bad567181083e98073f6
Sumit <sumeetbajra@gmail.com>
2017-03-14 02:10:33 -0700

    404 page added

--- public/js/app/pages/learn.js
+++ public/js/app/pages/learn.js
@@ -2,12 +2,11 @@
 $(document).ready(function() {
     api.kachha.list({
         success:function (d) {
-            console.log(d)
             $.each(d, function (i, v) {
                 if(!(i % 4)) {
                     $('#course_catalog').append('<div class="course_catalog_row"></div>')
                 }
-                $('.course_catalog_row:last').append('<div class="col-lg-3 col-md-6 col-sm-12 col-xs-12 wow fadeIn"><div class="video-wrapper course-catalog course-widget clearfix"><div class="post-media"><div class="entry entry-course"><a href="/learn/'+ v.class_uuid + '" style="background-image: url(\'http://files.edushala.com/course/' + v.class_uuid + '/img/sml.jpg\'); background-size: cover; background-repeat: no-repeat;"></a></div></div><!-- end media --><div class="widget-title clearfix"><h3><a href="/learn/'+ v.class_uuid + '">'+ v.class_name + '</a></h3><hr><div class="bottom-line clearfix"><div class="pull-left"><a class="readmore"><img src="" class="img-circle" alt="">'+ v.first_name +'</a></div><div class="pull-right"><a href="course-single.html" class="btn btn-sm btn-inverse"><span>Rs.</span>'+ v.price + '</a></div></div><!-- end bottom --></div><!-- end title --></div><!--widget --></div><!-- end col -->');
+                $('.course_catalog_row:last').append('<div class="col-lg-3 col-md-6 col-sm-12 col-xs-12 wow fadeIn"><div class="video-wrapper course-catalog course-widget clearfix"><div class="post-media"><div class="entry entry-course"><a href="/learn/'+ v.class_uuid + '" style="background-image: url(\'http://files.edushala.com/course/' + v.class_uuid + '/img/sml.jpg\'); background-size: cover; background-repeat: no-repeat;"></a></div></div><!-- end media --><div class="widget-title clearfix"><h3><a href="/learn/'+ v.class_uuid + '">'+ v.class_name + '</a></h3><hr><div class="bottom-line clearfix"><div class="readmore-ellipses pull-left"><a class="readmore"><img src="" class="img-circle" alt="">'+ v.first_name +'</a></div><div class="pull-right"><a href="course-single.html" class="btn btn-sm btn-inverse"><span>Rs.</span>'+ v.price + '</a></div></div><!-- end bottom --></div><!-- end title --></div><!--widget --></div><!-- end col -->');
             });
         },
         error:function () {
